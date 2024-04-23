9a156dfa6ef86b4bf188863b344708ffccc698b1
Advaith <advaith.narayanan20@gmail.com>
2024-04-23 14:58:29 -0700

    Added node modules

--- server/processing.py
+++ server/processing.py
@@ -14,26 +14,3 @@ def recog():
             return jsonify({'error': 'couldnt get dat.: '+str(e)}), 400
     else:
         return jsonify({'error': 'Invalid JSON in the request.'}), 400
-
-
-# sys.stderr.write("running")
-# with open("a.txt", "w") as file:
-#     # Write some content to the file
-#     file.write("Hello, world!")
-
-# def process_message(message):
-#     print('Message received:', message, "From client")
-
-# if __name__ == '__main__':
-
-#     print("running")
-#     sys.stderr.write("running")
-#     while True:
-#         message = sys.stdin.readline().strip()
-#         if message:            
-#             # Process the message
-#             process_message(message)
-            
-#             sys.stdout.flush() # NEEDED TO SEND IMMEDIATELY
-
-#         time.sleep(0.001) # STOP FOR 100 MILLISECONDS
